709fbe5177bc17335b63d183fc9da2cedc77345c
Isaac <isaacbrendel1@gmail.com>
2024-11-14 12:40:38 -0500

    docker

--- backend/members/apps.py
+++ backend/members/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class MembersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "members"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backend.members'  # Make sure this is the correct path
